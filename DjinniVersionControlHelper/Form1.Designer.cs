01971e994d27d29f9acb17eb849b16b0d01c8dc7
Lisu <lisembart97@gmail.com>
2019-03-10 22:51:19 +0100

    Added project files, Created UI

---
+++ DjinniVersionControlHelper/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DjinniVersionControlHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.setGitPathBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(487, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path to Djinni mod files: C:\\Users\\lisem\\AppData\\Local\\The Witcher\\temp\\__Resourc" +
+    "esUnpackTmp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(217, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Path to Git mod files: C:\\Projects\\Mods\\Test\r\n";
+            // 
+            // setGitPathBtn
+            // 
+            this.setGitPathBtn.Location = new System.Drawing.Point(384, 34);
+            this.setGitPathBtn.Name = "setGitPathBtn";
+            this.setGitPathBtn.Size = new System.Drawing.Size(114, 23);
+            this.setGitPathBtn.TabIndex = 2;
+            this.setGitPathBtn.Text = "Change Git Path";
+            this.setGitPathBtn.UseVisualStyleBackColor = true;
+            this.setGitPathBtn.Click += new System.EventHandler(this.setGitPathBtn_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(214, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Copy mod files to Git folder";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(285, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(214, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Copy mod files from Git folder";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 116);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.setGitPathBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Djinni Version Control Helper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button setGitPathBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
